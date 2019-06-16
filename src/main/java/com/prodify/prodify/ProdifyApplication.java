a442b4dab4cb0ccdeec44657685944f106fb5125
Mritunjay Yadav <mritunjay.yadav@mindfiresolutions.com>
2019-06-16 16:00:28 +0530

    creating new project

---
+++ src/main/java/com/prodify/prodify/ProdifyApplication.java
@@ -0,0 +1,13 @@
+package com.prodify.prodify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdifyApplication.class, args);
+	}
+
+}
